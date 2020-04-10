91205a5ea76b88784df242f28620046edce5e4c7
Joan Palacios <jopabel93@gmail.com>
2020-04-11 00:49:58 +0200

    Update beam-patterns.py
    plots

--- beam-patterns.py
+++ beam-patterns.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 class AntennaArray(object):
     def __init__(self, N_antennas=16, lambda_ratio=0.5):
@@ -16,7 +17,7 @@ class AntennaArray(object):
     # Beam-pattern creation (relative angle)
     def bp_sinc(self, width):
         half_antennas = (self.N_antennas-1)/2
-        return [np.sinc((ii-half_antennas)/np.pi) for ii in range(self.N_antennas)]
+        return [np.sinc((width/(2*np.pi))*(ii-half_antennas)) for ii in range(self.N_antennas)]
     def bp_roots(self, roots):
         bp_mat = [np.fft.fft(np.concatenate([[root], [1], np.zeros(len(roots)-1)])) for root in roots]
         return np.fft.ifft(np.prod(bp_mat, axis=0))
@@ -26,9 +27,9 @@ class AntennaArray(object):
         return self.bp_roots(roots)
     # Array response
     def array_response(self, bp, x):
-        return np.dot(bp, np.array(2j*np.pi*np.arange(self.N_antennas)[:, np.newaxis]*np.array(x)[np.newaxis, :]))
+        return np.dot(bp, np.exp(1j*np.arange(self.N_antennas)[:, np.newaxis]*np.array(x)[np.newaxis, :]))
 
-Antenna = AntennaArray(16, 0.5)
+Antenna = AntennaArray(256, 0.25)
 
 width = np.pi
 
@@ -40,5 +41,13 @@ Y = np.sin(angle_domain)
 bp_sinc = Antenna.bp_sinc(width)
 bp_cheby = Antenna.bp_cheby(width)
 
-R_sinc = Antenna.array_response(bp_sinc, angle_domain)
-R_cheby = Antenna.array_response(bp_cheby, angle_domain)
+R_sinc = np.abs(Antenna.array_response(bp_sinc, angle_domain))
+R_cheby = np.abs(Antenna.array_response(bp_cheby, angle_domain))
+r_max = np.max([R_sinc])
+
+plt.plot(X*R_sinc/r_max, Y*R_sinc/r_max)
+plt.xlim([-1, 1])
+plt.ylim([-1, 1])
+plt.show()
+
+time.sleep(10)
