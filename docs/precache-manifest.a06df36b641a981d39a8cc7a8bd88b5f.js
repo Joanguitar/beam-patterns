d50b183351119c5ea9bff3cfc8939815c5be09a7
Joan Palacios <jopabel93@gmail.com>
2020-04-11 16:05:42 +0200

    created reactjs folder and moved the build to docs

---
+++ docs/precache-manifest.a06df36b641a981d39a8cc7a8bd88b5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3ae0ee0133b2b966af6b15bc2271f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d29ebc9718ce95135a20",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ca1bf7f6ed5f8bf3fcbe",
+    "url": "/static/js/2.82384ee6.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/static/js/2.82384ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d29ebc9718ce95135a20",
+    "url": "/static/js/main.560134e4.chunk.js"
+  },
+  {
+    "revision": "70a945adc52861859a08",
+    "url": "/static/js/runtime-main.c31ba872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
