f63eb3977290d91d37f3f6208242249fcce1cdb5
Joan Palacios <jopabel93@gmail.com>
2020-04-11 20:33:12 +0200

    push docs

---
+++ docs/precache-manifest.637b3d9f15dcb4c8062528e0cf4068b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7003b04707cf8b98e5382181b38a8f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f689d4bf83c627f24c8e",
+    "url": "/static/css/main.83ba283e.chunk.css"
+  },
+  {
+    "revision": "c011675de1087b7889b2",
+    "url": "/static/js/2.b22dca65.chunk.js"
+  },
+  {
+    "revision": "5b6954ce485c64b08618a73eaa728067",
+    "url": "/static/js/2.b22dca65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f689d4bf83c627f24c8e",
+    "url": "/static/js/main.7b4a4c6f.chunk.js"
+  },
+  {
+    "revision": "70a945adc52861859a08",
+    "url": "/static/js/runtime-main.c31ba872.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  }
+]);
