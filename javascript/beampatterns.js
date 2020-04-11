7228b953ec808e09f1b262e03fe76d6e2a50e7b3
Joan Palacios <jopabel93@gmail.com>
2020-04-11 22:35:48 +0200

    Updated javascript version

--- javascript/beampatterns.js
+++ javascript/beampatterns.js
@@ -1,38 +1,47 @@
-import math from "math.js"
+import {range, exp, complex, subtract, multiply, dotMultiply, pi, zeros} from "mathjs"
 
-sinc(ang){
-  if (abs(ang) < 0.5) {
+function sinc(ang){
+  ang *= pi
+  if (Math.abs(ang) < 0.5) {
     // Taylor seris: 1-x^2/3!+x^4/5!-x^6/7!...
     const ang_2 = ang**2;                // x^2
     var curr_term = 1;                   // current term
+    var output = 1;                      // the Taylor series result
     for (var ii = 2; ii < 12; ii += 2) {
-      current_term *= -ang_2/(ii*(ii+1))
+      curr_term *= -ang_2/(ii*(ii+1))
       output += curr_term
     }
     return output
   } else {
-    return sin(ang)/ang
+    return Math.sin(ang)/ang
   }
+}
+
+function expi(x){
+  return(exp(complex(0, x)))
 }
 
 class AntennaArray{
   constructor(n_antennas, lambda_ratio){
     this.n_antennas = n_antennas;
     this.lambda_ratio = lambda_ratio;
-    this.antenna_index = math.range(0, n_antennas)
+    this.antenna_index = range(0, n_antennas)
   }
   bp_steer(bp, ang){
-    const steering_vector = math.exp(math.complex(0, math.multiply(this.antenna_index, ang)))
-    math.dotMultiply(bp, steering_vector)
+    const steering_vector = multiply(this.antenna_index, ang).map(element => expi(element));
+    return(dotMultiply(bp, steering_vector));
   }
   bp_sinc(width){
-    const sinc_index = math.multiply(this.antenna_index-(this.n_antennas-1)/2, width/(2*math.PI))
+    const sinc_index = multiply(subtract(this.antenna_index, (this.n_antennas-1)/2), width/(2*pi))
     return(sinc_index.map(item => sinc(item)))
   }
-  set_nag_domaing_rel(x){
+  set_ang_domaing_rel(x){
     this.ang_domain_rel = x;
-    exp_index = x.map(item => math.multiply(this.index, item))
-    this.response_domain_rel = math.exp(math.complex(0, exp_index))
+    const exp_index = x.map(item => multiply(this.antenna_index, item))
+    this.response_domain_rel = exp_index.map(item => expi(item))
+  }
+  array_response_rel(bp){
+    return(multiply(this.response_domain_rel, bp))
   }
 }
 
