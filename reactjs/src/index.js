6aa06f848d1d7345475ab2a345d8376f5cba3093
Joan Palacios <jopabel93@gmail.com>
2020-04-11 21:44:49 +0200

    IS_DEV

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -11,10 +11,12 @@ import "./assets/demo/demo.css";
 import "./assets/css/nucleo-icons.css";
 //document.body.classList.add("white-content");
 
+const IS_DEV = !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={'/directory-name'}>
-      <Route path={'/'} component={App} />
+      <Route path={IS_DEV ? '/' : '${process.env.PUBLIC_URL}/'} component={App} />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
