776bc0b5359cc9785b97cdad91b7ff0e6acb3779
Joan Palacios <jopabel93@gmail.com>
2020-04-11 21:20:47 +0200

    basename added

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -11,7 +12,8 @@ import "./assets/css/nucleo-icons.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router basename={'/directory-name'}/>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
