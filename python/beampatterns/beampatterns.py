6df0e7ca5bb0c8b551121af19f65d10bd6016319
Joan Palacios <jopabel93@gmail.com>
2020-04-12 21:43:03 +0200

    Updated python

--- python/beampatterns/beampatterns.py
+++ python/beampatterns/beampatterns.py
@@ -1,24 +1,49 @@
 import numpy as np
 
 class AntennaArray:
-    def __init__(self, N_antennas=16, lambda_ratio=0.5):
-        self.N_antennas = N_antennas
+    def __init__(self, n_antennas=16, lambda_ratio=0.5):
+        self.n_antennas = n_antennas
         self.lambda_ratio = lambda_ratio
-    # Angle transformations (abs from -pi to pi, rel from -2*pi*lambda_ratio to 2*pi*lambda_ratio)
-    def ang_rel2abs(self, ang):
-        return (2*self.lambda_ratio*np.pi)*np.sin(ang)
+        self.antenna_index = np.arange(0, n_antennas);
+        self.ang_const = 2*lambda_ratio*np.pi;
+    # Angle transformations (abs from -pi to np.pi, rel from -2*pi*lambda_ratio to 2*pi*lambda_ratio)
     def ang_abs2rel(self, ang):
         return np.asin(np.array(ang)/(2*self.lambda_ratio*np.pi))
+    def ang_abs2rel_2(self, ang):
+        my_ang = ang
+        output = 0
+        while my_ang > np.pi/2:
+            output += 2*self.ang_const
+            my_ang -= np.pi
+        while my_ang < -np.pi/2:
+            output -= 2*self.ang_const
+            my_ang += np.pi
+        return output + self.ang_const*np.sin(my_ang)
+    def ang_rel2abs_2(self, ang):
+        my_ang = ang
+        output = 0
+        while my_ang > self.ang_const:
+            output += np.pi
+            my_ang -= 2*self.ang_const
+        while my_ang < -self.ang_const:
+            output -= np.pi
+            my_ang += 2*self.ang_const
+        return output + np.asin(my_ang/self.ang_const)
     # Steering function
     def bp_steer(self, b, ang):
-        return [b_ii*np.exp(2j*ii*self.lambda_ratio*np.pi*ang) for b_ii, ii in zip(b, range(len(b)))]
+        return [b_ii*np.exp(2j*np.pi*ang) for b_ii, ii in zip(b, range(len(b)))]
     # Beam-pattern creation (relative angle)
     def bp_sinc(self, width):
-        half_antennas = (self.N_antennas-1)/2
-        return [np.sinc((width/(2*np.pi))*(ii-half_antennas)) for ii in range(self.N_antennas)]
+        half_antennas = (self.n_antennas-1)/2
+        return [np.sinc((width/(2*np.pi))*(ii-half_antennas)) for ii in range(self.n_antennas)]
     # Array response
     def set_ang_domain_rel(self, x):
         self.ang_domain_rel = x
-        self.response_domain_rel = np.exp(1j*np.range(self.N_antennas)[:, np.newaxis]*np.array(x)[np.newaxis, :])
+        self.response_domain_rel = np.exp(1j*self.antenna_index[:, np.newaxis]*np.array(x)[np.newaxis, :])
     def array_response_rel(self, bp):
         return np.dot(bp, self.response_domain_rel)
+    def set_ang_domain_abs(self, x):
+        self.ang_domain_rel = x
+        self.response_domain_abs = np.exp(1j*self.ang_const*self.antenna_index[:, np.newaxis]*np.sin(x)[np.newaxis, :])
+    def array_response_abs(self, bp):
+        return np.dot(bp, self.response_domain_abs)
--- python/example.py
+++ python/example.py
@@ -1,27 +1,94 @@
+# Imports
 import beampatterns
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-AntennaArray = beampatterns.AntennaArray
+# Antenna parameters
+n_antennas = 15
+lambda_ratio = 0.5
 
-Antenna = AntennaArray(256, 0.5)
+# Antenna definition
+antenna = beampatterns.AntennaArray(n_antennas, lambda_ratio)
 
-width = 0.5*np.pi
+# Angle domain for plots
+angle_domain = np.linspace(-np.pi, np.pi, 1024)
+antenna.set_ang_domain_rel(angle_domain)
+antenna.set_ang_domain_abs(angle_domain)
 
-angle_domain_rel = np.linspace(-np.pi, np.pi, 1024)
+# Circumference (for the polar plot)
+X = np.cos(angle_domain)
+Y = np.sin(angle_domain)
 
-X_rel = np.cos(angle_domain_rel)
-Y_rel = np.sin(angle_domain_rel)
+# Actual beam-pattern calculation
+center = 0      # Center in relative angle
+width = np.pi   # Width in relative angle
+####
+#       center, width can be computed from the relative angle sector
+#       extremes [a, b] as:
+#           center = (a + b)/2, width = b-a
+#
+#       a, b can be computed from the absolute angle sector extremes
+#       [aa, bb] as:
+#           a = antenna.ang_abs2rel_2(aa), b = antenna.ang_abs2rel_2(bb)
+####
+bp = antenna.bp_sinc(width)    # Unsteered sector of givend width
+bp = antenna.bp_steer(bp, center)  # Steer beam-pattern
 
-bp_sinc = Antenna.bp_sinc(width)
+# Power per angle
+R_rel = np.power(np.abs(antenna.array_response_rel(bp)), 2)
+R_abs = np.power(np.abs(antenna.array_response_abs(bp)), 2)
+# Maximum of this power
+r_max = np.max([R_rel, R_abs])
+# Normalization
+R_rel /= r_max
+R_abs /= r_max
 
-R_sinc = np.abs(Antenna.array_response(bp_sinc, angle_domain_rel))
-r_max = np.max(R_sinc)
-
-plt.plot(X_rel*R_sinc/r_max, Y_rel*R_sinc/r_max)
-plt.xlim([-1, 1])
-plt.ylim([-1, 1])
+# Plots
+fig = plt.figure(figsize=(8, 8))
+##### Natural
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+fig.suptitle('Beam-pattern')
+ax1.plot(X, Y, 'k', lw=1)
+ax2.plot(X, Y, 'k', lw=1)
+for k in [0.25, 0.5, 0.75]:
+    ax1.plot(k*X, k*Y, 'k', lw=0.5)
+    ax2.plot(k*X, k*Y, 'k', lw=0.5)
+ax1.plot(X*R_rel, Y*R_rel, lw=3)
+ax2.plot(X*R_abs, Y*R_abs, lw=3)
+ax1.set_xlim([-1, 1])
+ax1.set_ylim([-1, 1])
+ax1.set_aspect('equal')
+ax2.set_xlim([-1, 1])
+ax2.set_ylim([-1, 1])
+ax2.set_aspect('equal')
+#####  Logarithmic
+ax3 = fig.add_subplot(2, 2, 3)
+ax4 = fig.add_subplot(2, 2, 4)
+fig.suptitle('Beam-pattern')
+ax3.plot(X, Y, 'k', lw=1)
+ax4.plot(X, Y, 'k', lw=1)
+for k in [0.25, 0.5, 0.75]:
+    ax3.plot(k*X, k*Y, 'k', lw=0.5)
+    ax4.plot(k*X, k*Y, 'k', lw=0.5)
+######  Normalized to map [-40, 0] to [0, 1]
+R_rel = np.maximum(0.25*np.log10(R_rel)+1, 0)
+R_abs = np.maximum(0.25*np.log10(R_abs)+1, 0)
+ax3.plot(X*R_rel, Y*R_rel, lw=3)
+ax4.plot(X*R_abs, Y*R_abs, lw=3)
+ax3.set_xlim([-1, 1])
+ax3.set_ylim([-1, 1])
+ax3.set_aspect('equal')
+ax4.set_xlim([-1, 1])
+ax4.set_ylim([-1, 1])
+ax4.set_aspect('equal')
+# Titles
+ax1.set_title('Relative angle (Natural)')
+ax2.set_title('Absolute angle (Natural)')
+ax3.set_title('Relative angle (Logarithmic)')
+ax4.set_title('Absolute angle (Logarithmic)')
+# Show
 plt.show()
 
-time.sleep(10)
+time.sleep()
